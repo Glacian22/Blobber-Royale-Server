b3060d5a9d59d2024612ff9cabf3f63ee8c9e2f0
Quantum Philosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-12-03 15:04:47 -0800

    cleaning up server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// *****************************************************************************
+// ******************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server
 // ******************************************************************************
 
@@ -12,6 +12,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 // const passport = require('./config/passport')
 
+const PORT = process.env.PORT || 3030
+
 // Sets up the Express App
 // =============================================================
 const app = express()
@@ -19,30 +21,29 @@ const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-const PORT = process.env.PORT || 3030
-
 app.use(cors())
 
-// Sets up the Express app to handle data parsing
+// Setting up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// Sets up Express Sessions
+// Setting up Express Sessions
 app.use(session({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true
 }))
+
+// Setting up the Passport plugin for Express
 // app.use(passport.initialize())
 // app.use(passport.session())
 
-// Requiring our models for syncing
-// const db = require('./models')
-
-// Routes
+// Requiring routes
 // require('./routes').authRoutes(app)
 require('./routes').apiRoutes(app)
 
+// Setting up socket.io
+// =============================================================
 io.on('connection', function (socket) {
   console.log(socket.id, 'Connected!')
   socket.on('disconnect', function () {
@@ -50,12 +51,14 @@ io.on('connection', function (socket) {
   })
 })
 
-// Syncing our Mongoose models and then starting our Express app
+// Syncing our Mongoose models
 // =============================================================
 // TODO: check for test flag for MongoDB
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/blobber-royale'
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
 
+// Starting the Express app
+// =============================================================
 http.listen(PORT, function () {
   console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT)
 })
